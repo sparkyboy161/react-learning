81eac66edc9e5d3d888443a9b75da6a241f2bac6
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2019-11-19 08:57:17 +0700

    fix the lastest code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,13 @@
 import React , {Component} from 'react';
 import './TodoItem.css';
-import 'classnames'
+import classNames from 'classnames';
 
 class TodoItem extends Component{
     render(){
-        const classNames = require('classnames');
-        
         const {item} = this.props;
-        let className = classNames('TodoItem',{' TodoItem-complete': item.isComplete}); 
-        
         return(
-            <div className={className}>{this.props.item.title}</div>
+            <div className={classNames('TodoItem',{' TodoItem-complete': item.isComplete=== true})}>
+            {this.props.item.title}</div>
         );
     }
 }
